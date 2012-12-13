585a1b5c109c69bf2091350606450b858fc7e634
Daniel Koskinen <dani@dani.fi>
2012-12-13 10:24:28 +0200

    Initial plugin commit, doesn't do anything yet

---
+++ h1-contact-manager.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: H1 Contact Manager
+Plugin URI: http://h1.fi
+Description: Add and manage contacts
+Version: 0.1
+Author: Daniel Koskinen / H1
+Author URI: http://h1.fi
+License: GPL2
+*/
+/*  Copyright 2012  Daniel Koskinen (email : dani@h1.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
